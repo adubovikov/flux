d5ac518cfc7fa59c57ef335ec89dcdb1fc67edd1
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-11-03 00:18:36 +0100

    Create hash.go

---
+++ stdlib/contrib/qxip/hash/hash.go
@@ -0,0 +1,36 @@
+package hash
+
+import (
+	"context"
+
+	"github.com/influxdata/flux/runtime"
+	"github.com/influxdata/flux/semantic"
+	"github.com/influxdata/flux/values"
+)
+
+var hashFuncName = "hash"
+
+func init() {
+	SpecialFns = map[string]values.Function{
+     "test": values.NewFunction(
+        "test",
+        runtime.MustLookupBuiltinType("hash", "test"),
+        func(ctx context.Context, args values.Object) (values.Value, error) {
+          v, ok := args.Get("v")
+          if !ok {
+            return nil, errors.New(codes.Invalid, "missing argument v")
+          }
+
+          if !v.IsNull() && v.Type().Nature() == semantic.String {
+            value := v.Str()
+            return values.NewString(value), nil
+          }
+          return nil, errors.Newf(codes.Invalid, "cannot hash value %v", v)
+        },
+        false,
+      )
+  }
+  
+  runtime.RegisterPackageValue("hash", "test", SpecialFns["test"])
+
+}
